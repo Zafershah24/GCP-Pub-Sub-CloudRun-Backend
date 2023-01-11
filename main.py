b468522bc58bbe32056d205596679aa3876fc60a
zafer247 <zafershah247@gmail.com>
2023-01-12 01:28:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,8 +10,7 @@ os.environ.setdefault("GCLOUD_PROJECT", "df-szafersa-s")
 
 
 app = Flask(__name__)
-# [END run_pubsub_server_setup]
-# [END cloudrun_pubsub_server_setup]
+
 
 
 # [START cloudrun_pubsub_handler]
@@ -19,21 +18,30 @@ app = Flask(__name__)
 @app.route("/", methods=["POST"])
 def index():
     data = request.get_data()
-    data=data.decode("utf-8")
+    print("get_data: ")
     print(type(data))
+    data=data.decode("utf-8")
+    print("decode: ")
+    print(type(data))
+    print(data)
     data=json.loads(data)
     
     # data=request.get_json()
+    print("loads:")
     print(type(data))
+    
    
     # Decoding Base64 pub sub message
     pubsub_message = data["message"]
     if isinstance(pubsub_message, dict) and "data" in pubsub_message:
         data = base64.b64decode(pubsub_message["data"]).decode("utf-8").strip()
+    print("decoded:")
     print(data)
     print(type(data))
-    data = data.replace("'", '"') 
+    # 
+
     data=json.loads(data)
+    print("jsonifieD")
     print(type(data))
     print(data)
     
@@ -42,38 +50,21 @@ def index():
     # print(type(data))
     
     # prj_id=str(prj_id)
-    mss=data['message']
-    recipients=data['receivers']
+    mss=data["message"]
+    recipients=data["receivers"]
+    event_subject=data["event_subject"]
     print(type(recipients))
-    # query = f"""SELECT DISTINCT cdsid FROM `df-szafersa-s.event_db.denormal` where project_id={prj_id};"""
-    # print(prj_id)
-    # print(mss)
-    # query_job = client.query(query)
-    # a=[]
-    # for row in query_job:
-    #     a.append(str(row[0]))
+
         
-    emailsender.recipients_list_sender(recipients,mss)
+    emailsender.recipients_list_sender(recipients,mss,event_subject)
 
     return jsonify(mss,recipients)
 
 
-# [END run_pubsub_handler]
-# [END cloudrun_pubsub_handler]
-@app.route('/pst',methods=['POST'])
-def pst_api():
-    '''
-    post endpoint test
-    '''
-    data=""
 
-    data = request.get_json()
-    messge=data['message']
-    return jsonify({'output':'Hello '+ messge})
 
 if __name__ == "__main__":
     PORT = int(os.getenv("PORT")) if os.getenv("PORT") else 8080
 
-    # This is used when running locally. Gunicorn is used to run the
-    # application on Cloud Run. See entrypoint in Dockerfile.
+   
     app.run(host="127.0.0.1", port=PORT, debug=True)
