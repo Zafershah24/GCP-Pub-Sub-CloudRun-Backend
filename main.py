be34d4012c7711203408c5eae308bb8c2e2ddd90
zafer247 <zafershah247@gmail.com>
2023-02-09 22:03:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from flask import Flask, request, jsonify
 import emailsender
 import base64
 os.environ.setdefault("GCLOUD_PROJECT", "df-szafersa-s")
-
+import webex
 
 
 
@@ -21,47 +21,63 @@ def index():
     print("get_data: ")
     print(type(data))
     data=data.decode("utf-8")
-    print("decode: ")
-    print(type(data))
-    print(data)
+
     data=json.loads(data)
     
-    # data=request.get_json()
-    print("loads:")
-    print(type(data))
+
     
    
     # Decoding Base64 pub sub message
     pubsub_message = data["message"]
     if isinstance(pubsub_message, dict) and "data" in pubsub_message:
         data = base64.b64decode(pubsub_message["data"]).decode("utf-8").strip()
-    print("decoded:")
-    print(data)
-    print(type(data))
+ 
     # 
 
     data=json.loads(data)
-    print("jsonifieD")
-    print(type(data))
-    print(data)
+ 
     
-    #converting to Dictionary/JSON
-    # data = json.loads(data)
-    # print(type(data))
+
     
     # prj_id=str(prj_id)
     mss=data["message"]
     recipients=data["receivers"]
     event_subject=data["event_subject"]
     print(type(recipients))
+    
+    for recipient, notification_type in recipients.items():
+        if notification_type == "email":
+        # perform email action
+            try:
+                emailsender.recipients_list_sender(recipient,mss,event_subject)
+            except:
+                print("email sending failed")
+
+        elif notification_type == "webex":
+            # perform webex action
+            # print(f"Scheduling WebEx meeting with {recipient}")
+            try:
+                webex.webexpush(recipient,event_subject,mss)
+            except:
+                print("webex sending failed")
+        elif notification_type == "both":
+            # perform both actions
+            try:
+                emailsender.recipients_list_sender(recipient,mss,event_subject)
+                webex.webexpush(recipient,event_subject,mss)
+            except:
+                print("both webex and email sending failed")
+        else:
+            print("Sending failed xxxxxxxxxxxxxx")
+
 
         
-    emailsender.recipients_list_sender(recipients,mss,event_subject)
+    
 
+    
+
+    
     return jsonify(mss,recipients)
-
-
-
 
 if __name__ == "__main__":
     PORT = int(os.getenv("PORT")) if os.getenv("PORT") else 8080
